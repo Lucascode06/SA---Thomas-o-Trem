43a64708c805228c3a3a0cf48583ee9c32c7d68d
Kauan2007**  git config --global user.email <kauan_fernandes3@estudante.sesisenai.org.br>
2025-04-08 09:45:43 -0300

    add- esqueceu a senha

--- validacao.js
+++ validacao.js
@@ -2,7 +2,8 @@ function validarFormulario(){
     const usuario = document.getElementById('usuario').value.trim();
     const senha = document.getElementById('senha').value.trim();
 
+}
 
-
-
+function redirecionar() {
+    window.location.href = 'esqueceu-a-senha.html';
 }
