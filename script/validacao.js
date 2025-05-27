c8c67029a105ebcb5c3d2eeaf2ab58e13dcad02d
guifischeer <guilherme_florent@estudante.sesisenai.org.br>
2025-05-27 09:36:32 -0300

    ADD - Comentarios

--- script/validacao.js
+++ script/validacao.js
@@ -14,14 +14,16 @@
   localStorage.setItem("informacaoSalva", textoDigitado);
 }*/
 
+/* Vai para a página inicial */
 function voltarPagina() {
   window.location.href = "index.html";
 }
 
+/* Redireciona para a página de recuperação de senha */
 function voltarPagina2() {
   window.location.href = "EsqueceuASenha.html";
 }
-
+/* Confirma se as duas senhas novas são iguais */
 function validasenhanova() {
   if (document.getElementById("senhanova").value == document.getElementById("confsenhanova").value) {
     window.location.href = "index.html";
@@ -31,14 +33,17 @@ function validasenhanova() {
   }
 }
 
+/* Após login válido, redireciona para o painel */
 function validalogin(){
 window.location.href = "../public/dashboard.html";
 }
 
+/* Após email válido, segue para código de confirmação */
 function validaemail(){
 window.location.href = "../public/codigoconf.html";
 }
 
+/* Se código estiver certo, permite redefinir a senha */
 function validaconf(){
 window.location.href = "../public/redefinirsenha.html";
 }
