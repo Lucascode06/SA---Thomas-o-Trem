6f70d42b56ecf639731a1a72f754a70fd02f7c6b
Lucascode06 <dalcanalelucas06@gmail.com>
2025-05-30 12:27:08 -0300

    add - cmmit do icaro, ajudei eles amudar o regex

--- script/validacao.js
+++ script/validacao.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function(){
       valido = false;
     }
 
-    const emailRegex = /^[^s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
     if(!emailRegex.test(email)){
       document.getElementById("erroEmail").textContent = "E-mail inválido";
