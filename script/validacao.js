345dd70073ba86ba64bb0d70c5cee371746bce50
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-05-30 12:01:45 -0300

    ADD - VALIDAÇÃO

--- script/validacao.js
+++ script/validacao.js
@@ -1,28 +1,4 @@
-/*function validarFormulario() {
-  const usuario = document.getElementById('usuario').value.trim();
-  const senha = document.getElementById('senha').value.trim();
-}*/
 
-/*function enviarDados() {
-  const textoDigitado = document.getElementById("caixaTexto").value;
-  localStorage.setItem("informacaoSalva", textoDigitado);
-  window.location.href = "dados.html";
-}*/
-
-/*function enviarDadosLogin() {
-  const textoDigitado = document.getElementById("textoLogin").value;
-  localStorage.setItem("informacaoSalva", textoDigitado);
-}*/
-
-/* Vai para a página inicial */
-function voltarPagina() {
-  window.location.href = "index.html";
-}
-
-/* Redireciona para a página de recuperação de senha */
-function voltarPagina2() {
-  window.location.href = "EsqueceuASenha.html";
-}
 /* Confirma se as duas senhas novas são iguais */
 function validasenhanova() {
   if (document.getElementById("senhanova").value == document.getElementById("confsenhanova").value) {
@@ -33,18 +9,37 @@ function validasenhanova() {
   }
 }
 
-/* Após login válido, redireciona para o painel */
-function validalogin(){
-window.location.href = "../public/dashboard.html";
-}
+/*Validação*/
+document.addEventListener("DOMContentLoaded", function(){
+  
+  const formulario = document.getElementById("infologin");
+  formulario.addEventListener("submit", function(e){
+    e.preventDefault();
 
-/* Após email válido, segue para código de confirmação */
-function validaemail(){
-window.location.href = "../public/codigoconf.html";
-}
+    let valido = true;
+    const email = document.getElementById("email").value.trim();
+    const senha = document.getElementById("senha").value.trim();
 
-/* Se código estiver certo, permite redefinir a senha */
-function validaconf(){
-window.location.href = "../public/redefinirsenha.html";
-}
+    console.log(email);
+    console.log(senha);
 
+    if(senha.length < 3){
+      document.getElementById("erroSenha").textContent = "Senha inválida";
+      valido = false;
+    }
+
+    const emailRegex = /^[^s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailRegex.test(email)){
+      document.getElementById("erroEmail").textContent = "E-mail inválido";
+      valido = false;
+    }
+    if(valido){
+      alert("formulário enviado com sucesso");
+      formulario.reset();
+      window.location.href = "public/dashboard.html";
+    }
+
+  });
+
+});
