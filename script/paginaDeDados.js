d40062b1d6fba3581e1f252d7073d931ff386deb
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-10-28 08:50:33 -0300

    removi .js inuteis

--- script/paginaDeDados.js
+++
@@ -1,6 +0,0 @@
-// Quando a página carregar, busca o dado salvo e mostra no console
-window.onload = function() {
-    const informacaoSalva = localStorage.getItem("informacaoSalva");
-    console.log("Dado guardado:", informacaoSalva);
-
-};
